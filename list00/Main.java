1857da240d7518e4a545233e0dd61c34024037c7
Paulo Armando <parmando@c1r7s1.42luanda.com>
2025-02-20 18:43:46 +0100

    testando as minhas classes 12

--- list00/Main.java
+++ list00/Main.java
@@ -3,22 +3,25 @@ public class Main{
     public static void main(String[] args)
     {
         ListaTarefa tarefa = new ListaTarefa();
+        System.out.println("Total 1: "+tarefa.totalTarefas());
         tarefa.adicionarTarefa("comer");
         tarefa.adicionarTarefa("tomar cafe");
         tarefa.adicionarTarefa("codar");
         tarefa.adicionarTarefa("desenhar");
-
+        System.out.println("Total 2 : "+tarefa.totalTarefas());
         tarefa.obeterDescricoes();
 
         tarefa.removerTarefa("desenhar");
 
         System.out.println("\n\n +++++ DEPOIS DE REMOVER desenhar ++++++");
+        System.out.println("Total 3 : "+tarefa.totalTarefas());
         tarefa.obeterDescricoes();
 
         Tarefa wc = new Tarefa("ir ao wc");
         tarefa.adicionarTarefa(wc);
         System.out.println("\n\n +++++ DEPOIS DE ADD WC ++++++");
         tarefa.obeterDescricoes();
+        System.out.println("Total 4 : "+tarefa.totalTarefas());
 
 
     }
