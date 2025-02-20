8167ce34350dc2cb088c7d181bc5a64d1f43ee96
Paulo Armando <parmando@c1r7s1.42luanda.com>
2025-02-20 19:13:22 +0100

    Resolvendo o exercicio de carrinho de compra

---
+++ CarrinhoDeCompra/Item.java
@@ -0,0 +1,44 @@
+public class Item
+{
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade)
+    {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco)
+    {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade)
+    {
+        this.quantidade = quantidade;
+    }
+    public String getNome()
+    {
+        return this.nome;
+    }
+
+    public double getPreco()
+    {
+        return this.preco;
+    }
+
+    public int setQuantidade()
+    {
+        return this.quantidade;
+    }
+
+
+}
